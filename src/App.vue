c4dc1d916d45f74f48120799eac820ca8c77a510
Paul25995 <paul-koplin@web.de>
2021-03-15 17:34:09 +0100

    Revert "Revert "Savegame init""
    This reverts commit 14418cbd5e78cb5eba2fe95469c98721ef9ff116.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,60 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/traffic">Verkehr</router-link> |
-      <router-link to="/factory">Fabrik</router-link><span v-if="adminShow"> |
-      <router-link to="/admin">Admin</router-link></span>
+    <v-row>
+      <v-col cols="1" md="3"></v-col>
+      <v-col cols="9" md="6" id="nav">
+        <span v-if="setGame">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/traffic">Verkehr</router-link> |
+          <router-link to="/factory">Fabrik</router-link><span v-if="adminShow"> |
+          <router-link to="/admin">Admin</router-link></span>
+        </span>
+      </v-col>
+      <v-col cols="1"></v-col>
+      <v-col cols="2" md="2">
+        <br>
+        <v-dialog
+            transition="dialog-top-transition"
+            max-width="600"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+                color="#F2C800"
+                v-bind="attrs"
+                v-on="on"
+            >Spieldaten</v-btn>
+          </template>
+          <template v-slot:default="dialog">
+            <v-card>
+              <v-toolbar
+                  color="amber"
+                  dark
+                  style="color: #111416; font-weight: bold;"
+              >Du kannst Einstellungen für dein Spiel treffen.</v-toolbar>
+              <v-card-text>
+                <br>
+                <v-row>
+                  <v-col cols="12"><p>Der Name unter dem alles gespeichert wird: <b>{{spielstand?spielstand:'Kein Spielstand vorhanden'}}</b></p></v-col>
+                </v-row>
+                <v-row>
+                  <v-col cols="12"><v-text-field
+                        v-model="newSpielstand"
+                        label="Wie soll der Spielstand heißen?"
+                    ></v-text-field></v-col>
+                </v-row>
+              </v-card-text>
+              <v-card-actions class="justify-end">
+                <v-btn
+                    text
+                    @click="setSpielstand"
+                >Speichern</v-btn>
+              </v-card-actions>
+            </v-card>
+          </template>
+        </v-dialog>
+      </v-col>
+    </v-row>
+    <div id="">
     </div>
     <v-app style="background-color: transparent">
     <router-view />
@@ -18,6 +68,10 @@ export default {
   data() {
     return {
       adminShow: false,
+      setGame: true,
+      spielstand: '',
+      newSpielstand: '',
+      dialog: false,
     };
   },
   methods: {
@@ -28,11 +82,23 @@ export default {
         this.adminShow = true;
       }
     },
+    getSpielstand() {
+      this.spielstand = localStorage.getItem('spielstand');
+    },
+    setSpielstand() {
+      if (this.newSpielstand !== '' && this.newSpielstand !== ' ') {
+        localStorage.setItem('spielstand', this.newSpielstand);
+      }
+      this.newSpielstand = '';
+      location.reload()
+    }
   },
   created() {
     this.checkAdmin();
   },
   mounted() {
+    this.getSpielstand();
+    this.$store.dispatch("isSetGame");
     this.$store.dispatch("setAllRessources");
     this.$store.dispatch("setAllNodes");
     this.$store.dispatch("setAllTraffic");
